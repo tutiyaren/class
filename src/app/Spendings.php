6c921a4b0dc828ea0e7e62adb21d4fe2de759f1e
ren <r999b66k222@outlook.jp>
2024-03-12 06:07:31 +0900

    create: logic_3-1

---
+++ src/app/Spendings.php
@@ -0,0 +1,31 @@
+<?php
+namespace App;
+use PDO;
+
+interface SpendingsInterface
+{
+    public function fetchAllSpendings();
+}
+
+class Spendings implements SpendingsInterface
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = new PDO(
+            "mysql:host=mysql; dbname=tq_quest;",
+            "root",
+            "password"
+        );
+    }
+
+    public function fetchAllSpendings()
+    {
+        $sql = "SELECT * FROM spendings";
+        $statement = $this->pdo->prepare($sql);
+        $statement->execute();
+        $spendings = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $spendings;
+    }
+}
